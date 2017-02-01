dae3e585abe95d1f1cea5370174b2508faac32bd
Dana Luther <dluther@envisageinternational.com>
2017-02-01 14:18:54 -0500

    Demo app

---
+++ tests/fixtures/data/contacts.php
@@ -0,0 +1,11 @@
+<?php
+	return [
+		'sample1' => [
+			'name'  => 'John Doe',
+			'socks' => 'Paisley',
+		],
+		'sample2' => [
+			'name'  => 'Jane Q. Smith',
+			'socks' => 'Floral',
+		],
+	];
---
+++ tests/unit/LoginTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class LoginTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+    }
+}
